ead82ae1a2f3e17a0d6baf0d5d8a9eb7d457b215
jraghupa <jraghupa@cisco.com>
2019-02-08 00:27:56 +0530

    added to readme and corrected issues

--- wordFinder/__init__.py
+++ wordFinder/__init__.py
@@ -34,15 +34,14 @@ def check_all_permutations(scrabble_str, prefix="", suffix=""):
     for perm in set(perm_list):
         if wordnet.synsets(perm):
             word_list.append(perm)
-            print(perm)
 
 
-def find_all_words(all_chars, blanks=0, prefix="", suffix="", min_chars=4):
+def find_all_words(all_chars, blanks=0, prefix="", suffix="", min_chars=1):
     if min_chars > len(all_chars):
         raise ValueError("min_chars should be lesser than number of characters in all_chars")
 
     all_combinations = []
-    for i in range(min_chars, len(all_chars)):
+    for i in range(min_chars, len(all_chars) + 1):
         all_combinations.extend([''.join(x) for x in combinations(all_chars, i)])
     for combo in all_combinations:
         word_finder(combo, blanks, prefix, suffix)
@@ -50,5 +49,9 @@ def find_all_words(all_chars, blanks=0, prefix="", suffix="", min_chars=4):
 
 count = 0
 word_list = []
-find_all_words("siljonb", prefix="", suffix="g", min_chars=3)
+find_all_words("slaus", blanks=0, prefix="a", suffix="t", min_chars=3)
+word_list = set(word_list)
+
+for word in word_list:
+    print(word)
 print(str(len(word_list)) + ' words found')
